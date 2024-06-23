5f2d79c121df19888be56e37882c76b7b8ec0aef
Matthew McClain <mthommcclain@gmail.com>
2024-06-23 12:10:35 -0700

    attempt rollup externalize dompurify

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,11 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    rollupOptions: {
+      external: ['dompurify']
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
